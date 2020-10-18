4920f3e8d22ee0ee58e11d32787ce62a57862241
Mona Lisa <dharmikpatel7@hotmail.com>
2020-10-18 18:15:27 -0400

    .

--- js.js
+++ js.js
@@ -1,26 +1,29 @@
-// Code to get the current day
-var now = function(daily){
-    today = new Date();
-    var day = today.getDate();
-    var month = today.getMonth()+1; 
-    var year = today.getFullYear();
+// // Code to get the current day
+// //      it should refresh to the next day at midnight
+// //      link an online clock to the code?
+// var today = function({
+//     getDate()
+//     console.log(today)
+
+// })
+// // Code to get timeblocks 
+// //      create an array???
+
+//     var militaryTime = ["0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"]
+
+// // add color for past, current, future:
+// //      red- past
+// //      blue- current
+// //      white- future
+// // make each timeblock clickable
+// //      text box should pop up so user can write their event
+// //          store the new events into each timeblock
+// //          events should stay even if the page is refreshed
+// //      
     
-    if(day<10) day='0'+day;
-    if(month<10) month='0'+month;
-    return (month+daily+day+daily+year);
-    };
-    console.log(now('/'));
-   
-// Code to get timeblocks 
-
-var time = function(hour){
-    var militaryTime = ["0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"]
     
-    click.militaryTime()
-
-    console.log(time)
     
 
 
-}
+// }
 
