7f8d82a8bc9d4ec53dfdceabbabcb82deadb16b5
Mona Lisa <dharmikpatel7@hotmail.com>
2020-10-25 15:22:37 -0400

    .

--- js.js
+++ js.js
@@ -1,29 +1,28 @@
-// // Code to get the current day
-// //      it should refresh to the next day at midnight
-// //      link an online clock to the code?
-// var today = function({
-//     getDate()
-//     console.log(today)
+// loop over time blocks
+$(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-// })
-// // Code to get timeblocks 
-// //      create an array???
-
-//     var militaryTime = ["0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"]
-
-// // add color for past, current, future:
-// //      red- past
-// //      blue- current
-// //      white- future
-// // make each timeblock clickable
-// //      text box should pop up so user can write their event
-// //          store the new events into each timeblock
-// //          events should stay even if the page is refreshed
-// //      
-    
-    
-    
+    // check if we've moved past this time
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+    }
+    else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
+});
 
 
-// }
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val(); var time = $(this).parent().attr("id");
 
+
+        localStorage.setItem(time, value);
+    })
+})
